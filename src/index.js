ed53cd62c3feba5a95370673b6295608306f9392
Lynette <lyna8907@gmail.com>
2018-02-22 15:56:09 -0600

    added a list to dom

--- src/index.js
+++ src/index.js
@@ -3,5 +3,4 @@ const $ = require('jquery');
 let sayhello = () => console.log("Hello!!");
 sayhello();
 
-$("body").text("Hey Hey Hey!");
-$("body").html("<h1>This is an H1!</h1>");
+$("body").html("<h1>This is an H1!</h1><ol><li>Step One</li><li>Step Two</li><li>Step Three</li></ol>");

e6afebd5e028ed4b21f6ff678fec21b01b6ede8f
Lynette <lyna8907@gmail.com>
2018-02-22 20:03:08 -0600

    module export exercise complete

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const $ = require('jquery');
+const niceString = require('./say-hello.js');
 
 let sayhello = () => console.log("Hello!!");
 sayhello();
 
 $("body").html("<h1>This is an H1!</h1><ol><li>Step One</li><li>Step Two</li><li>Step Three</li></ol>");
+$("body").append("<h3>" + niceString() + "</h3>");
---
+++ src/say-hello.js
@@ -0,0 +1,3 @@
+let niceString = () => "This is a nice message!";
+
+module.exports = niceString;
